2983ff17377baaaab345e91a1a771f4a3617a3ea
AhmedKiser <ahmed.kiser@northsouth.edu>
2022-08-06 21:10:45 +0600

    reg fixed

--- reg.php
+++ reg.php
@@ -1,35 +1,43 @@
 <?php
-
-
-    if(isset($_POST['name'])){
+$insert = false;
+if(isset($_POST['name'])){
+    // Set connection variables
     $server = "localhost";
     $username = "root";
     $password = "";
-    $database = 'registration';
 
-    $conn = mysqli_connect($server,$username,$password,$database);
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
 
-
-    if(!$conn){
-        die("connection failure".mysqli_connect_error());
+    // Check for connection success
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
     }
-    echo "successfull";
+    // echo "Success connecting to the db";
 
+    // Collect post variables
     $name = $_POST['name'];
+    $gender = $_POST['gender'];
     $age = $_POST['age'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
-    $gender = $_POST['gender'];
+   
+    $sql = "INSERT INTO `registration`.`reg` (`name`, `age`, `gender`, `email`, `phone`,`dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', current_timestamp());";
+    // echo $sql;
 
+    // Execute the query
+    if($con->query($sql) == true){
+        // echo "Successfully inserted";
 
-    $sql = "INSERT INTO `reg` (`age`, `email`, `phone`, `gender`, `name`, `dt`) VALUES ('$age', '$email', '$phone', '$gender', '$name', current_timestamp());";
-    echo $sql;
-
-    if($conn->query($sql)==true){
-        echo "successfull";
+        // Flag for successful insertion
+        $insert = true;
     }
-    else 
-        echo "fail";
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+
+    // Close the database connection
+    $con->close();
 }
 ?>
 
@@ -37,24 +45,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
+    <title>Welcome to Travel Form</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Sriracha&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>welcome to registration page</h1>
-
+    <!-- <img class="bg" src="bg.jpg" alt="IIT Kharagpur"> -->
     <div class="container">
+        <h1>Welcome to registration form</h3>
+        <p>Enter your details and submit this form to confirm your participation in the trip </p>
+        <?php
+        if($insert == true){
+        echo "<p class='submitMsg'>Thanks for submitting your form</p>";
+        }
+    ?>
         <form action="reg.php" method="post">
-            <input type="text" name="Name" id="name" placeholder="enter your name">
-            <input type="text" name="age" id="age" placeholder="enter your age">
-            <input type="text" name="email" id="email " placeholder="enter your email">
-            <input type="text" name="phone" id="phone " placeholder="enter your phone">
-            <input type="text" name="gender" id="gender" placeholder="enter your gender">
-            <button class="btn" id="submit">submit</button>
-            <button class="btn" id="reset">reset</button>
+            <input type="text" name="name" id="name" placeholder="Enter your name">
+            <input type="text" name="age" id="age" placeholder="Enter your Age">
+            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
+            <input type="email" name="email" id="email" placeholder="Enter your email">
+            <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
+            <button class="btn">Submit</button> 
         </form>
     </div>
-    </body>
+    <script src="index.js"></script>
+    
+</body>
 </html>
